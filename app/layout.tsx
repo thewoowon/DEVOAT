422fb596f9fe294fc150b0f854a531a45597202b
thewoowon <thewoowon@naver.com>
2024-05-02 23:40:42 +0900

    feat: initialize

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DEVOAT",
+  description: "기술의 바다를 항해하는 개발자들의 위한 보트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
